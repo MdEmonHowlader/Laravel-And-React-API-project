cb1018c9bf7e5bedd309be3add6dea008a7aedfa
Md. Emon <115633465+MdEmonHowlader@users.noreply.github.com>
2025-01-02 19:14:31 +0600

    Add Home and Layout components; configure routing and Tailwind CSS

--- laravel_api_react/tailwind.config.js
+++ laravel_api_react/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+
+  content: [
+    './src/**/*.{html,jsx,js}',
+    './public/index.html',
+  ],
   theme: {
     extend: {},
   },
